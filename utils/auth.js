e965529d34f5e4716afafef2e30a2cd100c994fe
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-10-02 17:33:47 -0400

    Updated Push

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,9 @@
-nes (8 sloc)  167 Bytes
-
 const withAuth = (req, res, next) => {
-    if(!req.session.user_id) {
-        res.redirect('/login');
-    } else {
-        next();
-    }
+  if(!req.session.user_id) {
+      res.redirect('/login');
+  } else {
+      next();
+  }
 };
 
 module.exports = withAuth;
