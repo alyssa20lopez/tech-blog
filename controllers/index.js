e2f0efbfea5f7f7f1a5f0bb8a061bc22377d7dfa
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-10-02 16:27:44 -0400

    Updated Push

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const dashboardRoutes = require('./dashboardRoutes')
-const homeRoutes = require('./homeRoutes')
+const dashboardRoutes = require('./dashboardRoutes.js');
+const homeRoutes = require('./homeRoutes.js');
 
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
