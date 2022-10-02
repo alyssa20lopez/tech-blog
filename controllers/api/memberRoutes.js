9fcf1a239f7f6f16078a4f4c7b87234fdeb70cc9
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-10-02 10:55:18 -0400

    Updated  memberRoutes

--- controllers/api/memberRoutes.js
+++ controllers/api/memberRoutes.js
@@ -63,6 +63,23 @@ router.post('/logout', (req, res) => {
   }
 });
 
-router.delete('')
+router.delete('/member/:id', async (req, res) => {
+  try {
+    const memberData = await Member.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!memberData) {
+      res.status(404).json({ message: 'No member found with this id!' });
+      return;
+    }
+
+    res.status(200).json(memberData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
