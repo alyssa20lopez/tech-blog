a730de91a21e4beae0748f57758829d043fc99fe
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-10-05 20:31:21 -0400

    Added unique and validation to login

--- models/Member.js
+++ models/Member.js
@@ -19,6 +19,15 @@ Member.init(
     member_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
       type: DataTypes.STRING,
